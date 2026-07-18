ce32bd762f59bda4f214c7bd8b98272f13bda136
dagopietri <127136308+dagopietri@users.noreply.github.com>
2026-07-18 17:25:33 -0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_login import LoginManager
+from config import Config
+from app.models import db, load_user
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    login_manager.user_loader(load_user)
+
+    from app.auth.routes import auth_bp
+    from app.dashboard.routes import dashboard_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(dashboard_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
